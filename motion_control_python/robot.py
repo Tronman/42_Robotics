7c1c9ebf6ab231abb1341ff156ea29659a5f72ef
Tronman <u15200796@tuks.co.za>
2017-10-26 12:08:05 +0200

    added liftfoward functions

--- motion_control_python/robot.py
+++ motion_control_python/robot.py
@@ -8,4 +8,14 @@ def liftLeftLeg(ser):
 	setAngle(ser, 6, 105)
 
 def stepLeftForward(ser):
-	
+	setAngle(ser, 8, 80)
+	setAngle(ser, 7, 100)
+	setAngle(ser, 6, 95)
+	time.sleep(.5)
+	setAngle(ser, 3, 110)
+	time.sleep(.5)
+	setAngle(ser, 2, 80)
+	time.sleep(.5)
+	setAngle(ser, 1, 105)
+	setAngle(ser, 0, 90)
+	time.sleep(.5)
