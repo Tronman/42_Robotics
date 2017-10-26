2b0d91a79128358a32b3360ad72f1078665ad4c2
Tronman <u15200796@tuks.co.za>
2017-10-26 11:52:39 +0200

    started the ROSless motion code

---
+++ motion_control_python/robot.py
@@ -0,0 +1,11 @@
+def liftLeftLeg(ser):
+	setAngle(ser, 5, 70)
+	setAngle(ser, 0, 80)
+	time.sleep(.5)
+	setAngle(ser, 5, 90)
+	setAgnle(ser, 7, 110)
+	setAngle(ser, 8, 80)
+	setAngle(ser, 6, 105)
+
+def stepLeftForward(ser):
+	
